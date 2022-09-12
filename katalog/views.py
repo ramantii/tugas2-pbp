5327191e50baefaff25a734770c8b774491cc5cf
ramanti.prajna1 <ramanti.prajna@gmail.com>
2022-09-12 22:53:13 +0700

    apa yaa

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_item_katalog,
+        'nama' : 'Ramanti'
+    }
+    return render(request, "katalog.html", context)
--- project_django/urls.py
+++ project_django/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('katalog/',include('katalog.urls')),
 ]
 
 # TODO: Implement Routings Here
+
+
+
